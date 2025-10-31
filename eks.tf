2ee63f437a5d62e43fb13b6cf4f5f85659a3fdaa
kishansoni2726 <43043137+kishansoni2726@users.noreply.github.com>
2025-11-01 01:19:22 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,53 +1,62 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "~> 22.0"
 
   name               = "${var.project}-eks-cluster"
   kubernetes_version = "1.33"
-
-  addons = {
-    coredns = {}
-    eks-pod-identity-agent = {
-      before_compute = true
-    }
-    kube-proxy = {}
-    vpc-cni = {
-      before_compute = true
-    }
-  }
-
-  endpoint_public_access = true
-  enable_cluster_creator_admin_permissions = true
 
   vpc_id                   = aws_vpc.main.id
   subnet_ids               = aws_subnet.private_subnets[*].id
   control_plane_subnet_ids = aws_subnet.private_subnets[*].id
 
+  endpoint_public_access                  = true
+  enable_cluster_creator_admin_permissions = true
+
+  addons = {
+    coredns = {}
+    kube-proxy = {}
+    vpc-cni = { before_compute = true }
+    eks-pod-identity-agent = { before_compute = true }
+  }
+
+  eks_managed_node_group_defaults = {
+    # Prevent re-provisioning when scaling or tweaking small things
+    lifecycle = {
+      ignore_changes = [
+        desired_size,
+        scaling_config[0].desired_size,
+        instance_types,
+        labels,
+        taints,
+        tags,
+      ]
+    }
+  }
+
   eks_managed_node_groups = {
-    example = {
+    main = {
       ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = var.node_group_instance_types
 
-      min_size       = var.node_group_min_size
-      max_size       = var.node_group_max_size
-      desired_size   = var.node_group_desired_size
+      min_size     = var.node_group_min_size
+      max_size     = var.node_group_max_size
+      desired_size = var.node_group_desired_size
 
-      lifecycle = {
-        ignore_changes = [
-          desired_size,
-          scaling_config[0].desired_size,
-          tags,
-        ]
+      capacity_type = "ON_DEMAND"
+      labels = { role = "worker" }
+
+      tags = {
+        Name = "${var.project}-eks-node-group"
       }
     }
   }
 
   tags = {
     Terraform = "true"
+    Project   = var.project
   }
 }
 
-# Data sources required for Kubernetes provider authentication
 data "aws_eks_cluster" "cluster" {
   name       = module.eks.cluster_name
   depends_on = [module.eks]
