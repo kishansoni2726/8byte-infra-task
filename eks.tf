0ad8fe12e94c70394c094404bce4bd15d39c5264
kishansoni2726 <43043137+kishansoni2726@users.noreply.github.com>
2025-11-01 01:12:27 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -17,34 +17,31 @@ module "eks" {
   }
 
   endpoint_public_access = true
-
   enable_cluster_creator_admin_permissions = true
 
   vpc_id                   = aws_vpc.main.id
   subnet_ids               = aws_subnet.private_subnets[*].id
   control_plane_subnet_ids = aws_subnet.private_subnets[*].id
 
-eks_managed_node_groups = {
-  example = {
-    ami_type       = "AL2023_x86_64_STANDARD"
-    instance_types = var.node_group_instance_types
-    min_size       = var.node_group_min_size
-    max_size       = var.node_group_max_size
-    desired_size   = var.node_group_desired_size
+  eks_managed_node_groups = {
+    example = {
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = var.node_group_instance_types
 
-    lifecycle = {
-      ignore_changes = [
-        desired_size,
-        scaling_config[0].desired_size,
-        tags,
-      ]
+      min_size       = var.node_group_min_size
+      max_size       = var.node_group_max_size
+      desired_size   = var.node_group_desired_size
+
+      lifecycle = {
+        ignore_changes = [
+          desired_size,
+          scaling_config[0].desired_size,
+          tags,
+        ]
+      }
     }
   }
-}
 
-  }
-
-  
   tags = {
     Terraform = "true"
   }
@@ -52,14 +49,11 @@ eks_managed_node_groups = {
 
 # Data sources required for Kubernetes provider authentication
 data "aws_eks_cluster" "cluster" {
-  
   name       = module.eks.cluster_name
   depends_on = [module.eks]
-
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name       = module.eks.cluster_name
   depends_on = [module.eks]
-
 }
