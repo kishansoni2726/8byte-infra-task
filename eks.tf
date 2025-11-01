8296d2021beb9b70bb902329d0787ab1341d1c03
Kishan Lalvadiya <kishansoni45285@gmail.com>
2025-11-01 19:45:33 +0530

    Updated EKS code

--- eks.tf
+++ eks.tf
@@ -1,87 +1,60 @@
-#############################################
-# EKS Cluster (Control Plane only)
-#############################################
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.8"
+  version = "~> 21.0"
 
   name               = "${var.project}-eks-cluster"
-  cluster_version    = "1.31"
-  enable_irsa        = true
-  create_node_groups = false # disable internal node groups
+  kubernetes_version = "1.33"
+
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
 
   vpc_id                   = aws_vpc.main.id
   subnet_ids               = aws_subnet.private_subnets[*].id
   control_plane_subnet_ids = aws_subnet.private_subnets[*].id
 
-  cluster_endpoint_public_access = true
-  enable_cluster_creator_admin_permissions = true
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = var.node_group_instance_types
 
-  addons = {
-    coredns = {}
-    kube-proxy = {}
-    vpc-cni = {
-      before_compute = true
-    }
-    eks-pod-identity-agent = {
-      before_compute = true
+      min_size       = var.node_group_min_size
+      max_size       = var.node_group_max_size
+      desired_size   = var.node_group_desired_size
+      disk_size      = 30
+      force_update_version = false
+
     }
   }
 
   tags = {
     Terraform = "true"
-    Project   = var.project
   }
 }
 
-#############################################
-# EKS Node Group (Managed independently)
-#############################################
-resource "aws_eks_node_group" "example" {
-  cluster_name    = module.eks.cluster_name
-  node_group_name = "${var.project}-node-group"
-  node_role_arn   = module.eks.node_group_iam_role_arn
-  subnet_ids      = aws_subnet.private_subnets[*].id
-
-  ami_type       = "AL2023_x86_64_STANDARD"
-  instance_types = var.node_group_instance_types
-  disk_size      = 20
-
-  scaling_config {
-    desired_size = var.node_group_desired_size
-    min_size     = var.node_group_min_size
-    max_size     = var.node_group_max_size
-  }
-
-  lifecycle {
-    ignore_changes = [
-      scaling_config[0].desired_size,
-      instance_types,
-      tags,
-    ]
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-
-  tags = {
-    Terraform = "true"
-    Project   = var.project
-  }
-
-  depends_on = [module.eks]
-}
-
-#############################################
-# Data Sources for Kubernetes Provider
-#############################################
+# Data sources required for Kubernetes provider authentication
 data "aws_eks_cluster" "cluster" {
+  
   name       = module.eks.cluster_name
   depends_on = [module.eks]
+
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name       = module.eks.cluster_name
   depends_on = [module.eks]
+
 }
